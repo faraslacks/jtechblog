2939f334770202091c16d943077d0081c71c5f3e
faraslacks <faraslacks@gmail.com>
2014-02-06 22:48:57 +0900

    Unused Spring configuration XMLs removed.

--- src/main/java/info/coffeeonkeyboard/web/MainController.java
+++ src/main/java/info/coffeeonkeyboard/web/MainController.java
@@ -8,11 +8,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * User: faraslacks
  * Date: 28.08.13
  * Time: 21:03
- * Main web of application.
+ * Main controller of application.
  */
 
 @Controller
 public class MainController {
+
+    private int i;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     String mapBlog() {

70456b5fb864fab06732060ffc0949c1e9f6bed8
faraslacks <faraslacks@gmail.com>
2014-02-08 12:42:10 +0900

    Spring-hibernate configuration removed.

--- src/main/java/info/coffeeonkeyboard/dao/BlogsDaoJdbcImpl.java
+++ src/main/java/info/coffeeonkeyboard/dao/BlogsDaoJdbcImpl.java
@@ -2,12 +2,16 @@ package info.coffeeonkeyboard.dao;
 
 import info.coffeeonkeyboard.domain.Blog;
 
+import java.sql.Connection;
+
 /**
  * User: numlock
  * Date: 28.01.14
  * Time: 11:58
  */
 public class BlogsDaoJdbcImpl implements BlogsDao {
+
+    private Connection connection;
 
     @Override
     public Blog getBlog(String name) {
